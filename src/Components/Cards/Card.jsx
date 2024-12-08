33ab86d9ab4dd95f7aa831df61ff81b55fbd2e58
OMKAR PAPANI <papaniomkar@gmail.com>
2024-12-08 09:46:15 +0530

    Update Card.jsx

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -11,8 +11,8 @@ function Card({data}) {
            const id = {idMeal}
             return(
             <>
-                <div key={idMeal} className='flex flex-col border p-6 bg-white 2xl:w-96 xl:w-80 lg:w-64 md:w-56 sm:w-[30%] w-64 rounded-xl'>
-                    <img src={strMealThumb} alt={strMeal}  className='2xl:w-80 2xl:h-80 xl:w-72 xl:h-72 lg:w-60 lg:h-60 md:w-48 md:h-48 sm:w-40 sm:h-40 w-60 h-60'/>
+                <div key={idMeal} className='flex flex-col border p-6 bg-white 2xl:w-96 xl:w-72 lg:w-64 md:w-56 sm:w-[30%] w-64 rounded-xl'>
+                    <img src={strMealThumb} alt={strMeal}  className='2xl:w-80 2xl:h-80 xl:w-64 xl:h-64 lg:w-60 lg:h-60 md:w-48 md:h-48 sm:w-40 sm:h-40 w-60 h-60'/>
                     <h2 className='text-xl text-left font-semibold mt-4 text-customBrown truncate'>{strMeal ? (strMeal.length > 22 ? strMeal.slice(0, 22) + '..' : strMeal) : ''}</h2>
                     <div className='flex flex-wrap justify-around w-full mt-4'>
                         <p className='text-xs font-semibold text-gray-500'>{strArea ? (strArea.toUpperCase()): ''}</p>
